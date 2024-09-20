312078cf36db9bdb8ce7d52055ed699e9de6745c
Nelly <nelsongael.diaz@gmail.com>
2024-09-20 15:49:13 -0400

    Added PA2 folder and files

---
+++ PA 2/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Node
+{
+    int data;
+    struct Node* next;
+}Node;
+
+int main(void)
+{
+    
+    printf("Hello World!\n");
+
+
+    return 0;
+}
